91cbe93539f73f8bd568faf5327aaad8b901a6d3
Chris Blyth <blythmeister@gmail.com>
2023-07-30 20:30:08 +0100

    Fix companion queue size

--- src/366x366/companion/index.js
+++ src/366x366/companion/index.js
@@ -216,7 +216,7 @@ function sendWeather(unit) {
 }
 
 function sendPong() {
-  msgq.send("pong", {size:msgq.getQueueSize()-1});
+  msgq.send("pong", {size:msgq.getQueueSize()});
 }
 
 function locationChange(initial) {
