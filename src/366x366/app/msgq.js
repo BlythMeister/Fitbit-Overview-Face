f0c453d9711260d341a21d3b15df953337d9f414
Chris Blyth <blythmeister@gmail.com>
2023-08-08 08:54:54 +0100

    Build fix

--- src/366x366/app/msgq.js
+++ src/366x366/app/msgq.js
@@ -78,9 +78,9 @@ function enqueue(messageKey, message, timeout = 600000) {
 function dequeue(id, messageKey) {
   if (id) {
     var dequeueResult = false;
-    for (var i = 0; i < queue.length; i++) 
+    for (var i = data.length-1; i >= 0; i--) {
       if (queue[i].id === id) {
-        queue.splice(i--, 1);
+        queue.splice(i, 1);
         dequeueResult = true;
         break;
       }
@@ -93,11 +93,11 @@ function dequeue(id, messageKey) {
       console.log(`Unable to dequeue message ${id} - QueueSize: ${queue.length}`);
     }
   } else if (messageKey) {
-    for (var i = 0; i < queue.length; i++) 
+    for (var i = data.length-1; i >= 0; i--) {
       var messageId = queue[i].id;
       var key = messageId.split("_")[0];
       if (key === messageKey) {
-        queue.splice(i--, 1);
+        queue.splice(i, 1);
         if (debugMessages) {
           console.log(`Dequeued message ${messageId} from key ${messageKey} - QueueSize: ${queue.length}`);
         }
